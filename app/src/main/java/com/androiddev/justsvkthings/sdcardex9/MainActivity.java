a02ead9189dd075724b9643a97cb1a785ccfa236
Sri-Vishnu-Kumar-K <srivishnukumar.k@gmail.com>
2016-03-12 15:00:34 +0530

    Initial commit

---
+++ app/src/main/java/com/androiddev/justsvkthings/sdcardex9/MainActivity.java
@@ -0,0 +1,79 @@
+package com.androiddev.justsvkthings.sdcardex9;
+
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final TextView textView = (TextView)findViewById(R.id.writing);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    String filename = "abc.txt";
+                    File myFile = new File(Environment.getExternalStorageDirectory(), filename);
+                    if (!myFile.exists())
+                        myFile.createNewFile();
+                    FileOutputStream fos;
+                    String string = "Just something random";
+                    byte[] data = string.getBytes();
+                    try {
+                        fos = new FileOutputStream(myFile);
+                        fos.write(data);
+                        fos.flush();
+                        fos.close();
+                        textView.setText("Write Complete");
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
